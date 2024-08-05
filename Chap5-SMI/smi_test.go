dde11fe63989d2a6473351e0df38b4f56de2104a
Davidovtch <eriq2077@gmail.com>
2024-08-05 20:25:21 -0300

    Folders names changed;Start of Chapter 5

---
+++ Chap5-SMI/smi_test.go
@@ -0,0 +1,34 @@
+package smi
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	t.Run("rectangle area", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := Area(rectangle)
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+	t.Run("circle area", func(t *testing.T) {
+		circle := Circle{10}
+		got := Area(circle)
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+}
