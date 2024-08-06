04659f72b55ca683e397bdef995ea272dd099c59
Davidovtch <eriq2077@gmail.com>
2024-08-06 18:13:53 -0300

    Chapter 5 finished

--- Chap5-SMI/smi_test.go
+++ Chap5-SMI/smi_test.go
@@ -13,22 +13,49 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangle area", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		got := Area(rectangle)
-		want := 72.0
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
-	t.Run("circle area", func(t *testing.T) {
-		circle := Circle{10}
-		got := Area(circle)
-		want := 314.1592653589793
+	checkArea := func(t testing.TB, s Shape, want float64) {
+		t.Helper()
+		got := s.Area()
 
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
+	}
+	t.Run("rectangle area", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		want := 72.0
+
+		checkArea(t, rectangle, want)
+
 	})
+	t.Run("circle area", func(t *testing.T) {
+		circle := Circle{10}
+		want := 314.1592653589793
+
+		checkArea(t, circle, want)
+	})
+}
+
+/*table test good for when you need to test implementations
+of a interface of it the data being
+passed needs lots of requirements*/
+
+func TestArea2(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		//the fields were named only to increase readability
+		//they need to be the same as the struct
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+		}
+	}
 }
