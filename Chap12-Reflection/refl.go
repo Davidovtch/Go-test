5d566be05d5f08a51c0f5201ae0ba3ba6a6055c3
Davidovtch <eriq2077@gmail.com>
2024-09-05 18:24:00 -0300

    Finished chapter 12

--- Chap12-Reflection/refl.go
+++ Chap12-Reflection/refl.go
@@ -8,17 +8,39 @@ func walk(x interface{}, fn func(input string)) {
 
 	val := getValue(x)
 
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
-		}
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
 	}
 
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walk(val.MapIndex(key).Interface(), fn)
+		}
+	case reflect.Chan:
+		for {
+			if v, ok := val.Recv(); ok {
+				walkValue(v)
+			} else {
+				break
+			}
+		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walkValue(res)
+		}
+	}
 }
 
 func getValue(x interface{}) reflect.Value {
--- Chap12-Reflection/refl_test.go
+++ Chap12-Reflection/refl_test.go
@@ -60,6 +60,22 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"Chris", "London"},
 		},
+		{
+			"slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Austin"},
+			},
+			[]string{"London", "Austin"},
+		},
+		{
+			"arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Austin"},
+			},
+			[]string{"London", "Austin"},
+		},
 	}
 
 	for _, test := range cases {
@@ -73,5 +89,71 @@ func TestWalk(t *testing.T) {
 				t.Errorf("got %q want %q", got, test.ExpectedCalls)
 			}
 		})
+	}
+
+	t.Run("maps", func(t *testing.T) {
+		m := map[string]string{
+			"Cow":   "Moo",
+			"Sheep": "Baa",
+		}
+
+		var got []string
+		walk(m, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Moo")
+		assertContains(t, got, "Baa")
+	})
+
+	t.Run("channels", func(t *testing.T) {
+		var c = make(chan Profile)
+
+		go func() {
+			c <- Profile{33, "Berlin"}
+			c <- Profile{34, "Paris"}
+			close(c)
+		}()
+
+		var got []string
+		want := []string{"Berlin", "Paris"}
+
+		walk(c, func(input string) {
+			got = append(got, input)
+		})
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v expected:%v", got, want)
+		}
+	})
+
+	t.Run("function", func(t *testing.T) {
+		fn := func() (Profile, Profile) {
+			return Profile{33, "Berlin"}, Profile{34, "Paris"}
+		}
+		var got []string
+		want := []string{"Berlin", "Paris"}
+
+		walk(fn, func(input string) {
+			got = append(got, input)
+		})
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v expected:%v", got, want)
+		}
+
+	})
+}
+
+func assertContains(t testing.TB, haystack []string, needle string) {
+	t.Helper()
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %v to contain %q but it didn't", haystack, needle)
 	}
 }
